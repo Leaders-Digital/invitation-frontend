dec9e6ebc5bd178d9e31740a9d58eef8760c944d
khalil <mohamedkhalil.kouroghli@gmail.com>
2024-10-22 15:42:08 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // i want to be able to rediect me to any path 
+  base: '/',
+
 })
