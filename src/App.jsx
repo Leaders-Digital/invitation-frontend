e59cde7c96beee3f9fd07d508fee9f2b2fa27ab5
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2024-10-22 15:22:42 +0200

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Form from "./Form";
 import User from "./User";
+import Hello from "./Hello";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/user/:id" element={<User />} />
+        <Route path="/hello" element={<Hello />} />
       </Routes>
     </Router>
   );
